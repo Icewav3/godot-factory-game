61edb48337e9d0b0daad90387c91699ec814f2c4
Cole Dorman <coledorman12@gmail.com>
2025-04-05 19:16:37 -0500

    Wip adding buildable inventory

--- Assets/Scenes/Buildables/Drills/BaseDrill.gd
+++ Assets/Scenes/Buildables/Drills/BaseDrill.gd
@@ -4,7 +4,9 @@ class_name BaseDrill
 
 @export var mining_rate: float = 1.0   # Units per extraction cycle
 @export var fuel_usage: float = 0.5    # Amount of fuel consumed per cycle
-
+@export var capacity: int = 10
+# local isntance vars
+var mined_material: MaterialData
 # Optionally, you can add more common properties or functions later
 func consume_fuel(fuel_amount: float) -> bool:
 	#TODO
