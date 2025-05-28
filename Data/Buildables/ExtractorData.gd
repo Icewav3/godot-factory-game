0d1ce692b69c41310f229bb0d4b9ec718c23288c
Cole Dorman <coledorman12@gmail.com>
2025-05-27 18:22:15 -0700

    Data structures created

---
+++ Data/Buildables/ExtractorData.gd
@@ -0,0 +1,7 @@
+extends buildable_data
+
+class_name extractor_data
+
+@export var extraction_interval: float
+@export var maximum_hardness: int = 0
+@export var consumed_resources: Dictionary[MaterialData, int] = {}
--- Data/Materials/MaterialData.gd
+++ Data/Materials/MaterialData.gd
@@ -6,3 +6,5 @@ class_name MaterialData
 @export var flammability: float
 @export var volatility: float
 @export var cooling_factor: float
+@export var sprite: Texture2D
+@export var hardness: int = 0
