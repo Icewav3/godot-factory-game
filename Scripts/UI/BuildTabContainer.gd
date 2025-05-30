1bcb524e0f902a6cab74ca068bbf4ec72c319a48
Cole Dorman <coledorman12@gmail.com>
2025-05-29 17:55:49 -0700

    🥰

---
+++ Scripts/UI/BuildTabContainer.gd
@@ -0,0 +1,15 @@
+extends TabContainer
+
+func _ready():
+	var drills_icon = preload("res://Assets/UI/TheNounProject/Drill.png")
+	var factories_icon = preload("res://Assets/UI/TheNounProject/Factory.png")
+	
+	set_tab_icon(0, drills_icon)
+	set_tab_icon(1, factories_icon)
+
+	# Optional: remove tab text for clean icons-only look
+	set_tab_title(0, "")
+	set_tab_title(1, "")
+	
+	set_tab_icon_max_width(0, 64)
+	set_tab_icon_max_width(1, 64)
