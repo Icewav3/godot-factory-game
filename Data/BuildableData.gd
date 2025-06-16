1fab67bab314bab564ce38e4b0db38349436c17d
Cole Dorman <coledorman12@gmail.com>
2025-06-16 16:47:11 -0700

    Shader test

--- Data/BuildableData.gd
+++ Data/BuildableData.gd
@@ -2,8 +2,9 @@ extends Resource
 
 class_name buildable_data
 
+@export_category("Buildable Info")
 @export var building_name: String = "Placeholder"
 @export var health: int = 100
-@export var required_resources: Dictionary[MaterialData, int] = {}
 @export var inventory_capacity: int = 10
+@export var required_resources: Dictionary[MaterialData, int] = {}
 @export var sprite: Texture2D
