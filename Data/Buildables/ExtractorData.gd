660933ea1adc326e7d71d23be35ac3aabae9b543
Cole Dorman <coledorman12@gmail.com>
2025-06-24 22:10:28 -0700

    Updated Refrences and deleted old base extractor class

--- Data/Buildables/ExtractorData.gd
+++ Data/Buildables/ExtractorData.gd
@@ -6,7 +6,7 @@ class_name ExtractorData
 @export var extraction_amount: int = 1
 @export var maximum_hardness: int = 0
 @export var consumed_resources: Dictionary[MaterialData, int] = {}
-const SCENE := preload("res://Prefabs/Buildables/Extractor.tscn")
+const SCENE := preload("res://Prefabs/Buildables/Extractor/Extractor.tscn")
 
 func get_scene() -> PackedScene:
 	return SCENE
