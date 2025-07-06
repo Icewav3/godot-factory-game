0d4c89901e20306a4a064ce2d406862108c1dd66
Cole Dorman <coledorman12@gmail.com>
2025-07-05 17:26:04 -0700

    fixed drones being goofed

--- Data/Buildables/DroneBayData.gd
+++ Data/Buildables/DroneBayData.gd
@@ -3,7 +3,7 @@ extends BuildableData
 class_name DroneBayData
 
 @export var drone_amount: int = 4
-@export var drone_speed: int = 100
+@export var drone_speed: int = 200
 @export var drone_inventory_capacity: int = 10
 @export var dock_radius: float = 150
 const SCENE := preload("res://Prefabs/Buildables/Logistics/DroneBay.tscn")
