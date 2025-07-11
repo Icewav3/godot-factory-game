0af254fa43bc08cd0fed621038d3ea671081fd91
Cole Dorman <coledorman12@gmail.com>
2025-07-11 10:17:43 -0700

    dialogue prep 🔥

---
+++ Prefabs/Game/Player.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var acceleration: float = 1
+@export var max_speed: float = 3
+@export var drag: float = 0.95
+
+var velocity: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	var input_vector := get_input().normalized()
+	
+	# Accelerate if input is given
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * acceleration * delta
+	# Clamp to max speed
+	if velocity.length() > max_speed:
+		velocity = velocity.normalized() * max_speed
+	# Apply drag
+	velocity *= drag
+	
+	# Update pos
+	position += velocity * delta
+
+func get_input() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
